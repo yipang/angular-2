56c00bd564001e22f68480154a0d10fadc5220e6
Yi Pang <pangyibobo@gmail.com>
2015-11-02 14:18:31 -0800

    lab0

--- data-request/js/main.js
+++ data-request/js/main.js
@@ -1,4 +1,9 @@
 // Create application
-
+var myApp = angular.module('myApp', [])
 
 // Bind controller customersCtrl
+myApp.controller('cutomersCtrl', function($scope, $http){
+    $http.get('http://www.w3schools.com/angular/customers.php').then(function(response)) {
+        $scope.name = response.data.records;
+    }
+})
--- mad-libs/js/main.js
+++ mad-libs/js/main.js
@@ -1,8 +1,10 @@
 // Create application
-
+var madApp = angular.module('madApp',[]);
 
 // Bind controller madCtrl
-
+myApp.controller('madCtrl', function($scope) {
+    $scope.name = "Andrew";
+});
 
 	// Set initial values for name, age, location, and food
 
