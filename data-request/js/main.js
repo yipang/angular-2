b623e190325276411995368a02ef1fa0d26491e1
mkfreeman <michael.k.freeman@gmail.com>
2015-11-01 19:34:57 -0800

    Removed answers from data-request

--- data-request/js/main.js
+++ data-request/js/main.js
@@ -1,10 +1,4 @@
 // Create application
-var myApp = angular.module('myApp', [])
 
-// Bind controller myCtrl
-myApp.controller('customersCtrl', function($scope, $http){
-	$http.get('http://www.w3schools.com/angular/customers.php').then(function(request) {
-		$scope.names = request.data.records
-		console.log(request.data)
-	})
-})
+
+// Bind controller customersCtrl
