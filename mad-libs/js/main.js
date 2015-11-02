92cff3a812dd19550e06874a4ce47df78e674682
mkfreeman <michael.k.freeman@gmail.com>
2015-11-01 23:04:01 -0800

    Removed answer from mad-libs

--- mad-libs/js/main.js
+++ mad-libs/js/main.js
@@ -1,12 +1,8 @@
 // Create application
-var myApp = angular.module('myApp', []);
+
 
 // Bind controller madCtrl
-myApp.controller('madCtrl', function($scope) {
 
-// Set initial values for name, age, location, and food
-	$scope.name = "Laura";
-	$scope.age = 62;
-	$scope.location = "Ohio";
-	$scope.food = 'Peanut Butter & Jelly';
-});
+
+	// Set initial values for name, age, location, and food
+
